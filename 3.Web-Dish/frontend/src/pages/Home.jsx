21f1d09a6a857c989856e5536de535f4791bad8f
git-since2005 <84366921+git-since2005@users.noreply.github.com>
2024-11-05 13:48:27 +0530

    Home.jsx search updated

--- 3.Web-Dish/frontend/src/pages/Home.jsx
+++ 3.Web-Dish/frontend/src/pages/Home.jsx
@@ -5,15 +5,29 @@ import NavBarDishes from '../components/NavBarDishes'
 import Choices from '../components/Choices/Choices'
 import SeasonalTop from '../components/Seasonal-Top/SeasonalTop'
 import Footer from '../components/FooterItem/Footer'
+import DishComponent from "../components/DishComponent"
+import '../components/DishShow.css'
+import '../components/results.css'
 
-const Home = () => {
+let i = 0
+const Home = (props) => {
   return (
-    <div className='overflow-x-hidden'>
-      <NavBarDishes />
-      <Banner />
-      <Choices />
-      <SeasonalTop />
-      <Footer />
+    <div className='overflow-x-hidden' >
+      <div style={{"display":`${props.showSearch == false?"none":"block"}`}}>
+        {props.dishes.map((e)=>{
+          console.log(e)
+          return(
+            <DishComponent key={i++} ingre={e.ingredients} instructions={e.instructions} title={e.dish_name} img={e.image} />
+          )
+        })}
+      </div>
+      <div style={{"display":`${props.showSearch == true?"none":"block"}`}}>
+        <NavBarDishes />
+        <Banner />
+        <Choices />
+        <SeasonalTop />
+        <Footer />
+      </div>
     </div>
   )
 }
