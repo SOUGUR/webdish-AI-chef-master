15d00dfe6b3ca41e7ff71d296f17e8c6c1e8ff7a
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2024-11-25 15:36:27 +0530

    Share Modal fix

--- 3.Web-Dish/frontend/src/components/DishPage/DishCookComponent.jsx
+++ 3.Web-Dish/frontend/src/components/DishPage/DishCookComponent.jsx
@@ -26,16 +26,22 @@ const RecipeSteps = ({ dish, people, steps }) => {
     const [ingredients, setIngredients] = useState([]);
 
     useEffect(() => {
-        const splitVal = dish.instructions[currentStep].step.split(" ");
+        // const splitVal = dish.instructions[currentStep].step.split(" ");
+        console.log(currentStep);
+        console.log(dish.instructions);
+        let stepIngredients = dish.instructions[currentStep]?.stepIngredients.map((item)=> item);
 
-        dish.ingredients.map((ingredient) =>
-            splitVal.forEach(element => {
-                if (element.substring(0, ingredient.name.length / 2 + 1) == ingredient.name.toLowerCase().substring(0, ingredient.name.length / 2 + 1)) {
-                    console.log(ingredient.name);
-                    setIngredients(prev => [...prev, ingredient.name]);
-                }
-            })
-        );
+        // dish.ingredients.map((ingredient) =>
+        //     splitVal.forEach(element => {
+        //         if (element.substring(0, ingredient.name.length / 2 + 1) == ingredient.name.toLowerCase().substring(0, ingredient.name.length / 2 + 1)) {
+        //             console.log(ingredient.name);
+        //             setIngredients(prev => [...prev, ingredient.name]);
+        //         }
+        //     })
+        // );
+        console.log("step ingredient")
+        console.log(stepIngredients);
+        setIngredients(stepIngredients);
     }, [currentStep]);
 
     const goToNextStep = () => {
@@ -110,13 +116,13 @@ const RecipeSteps = ({ dish, people, steps }) => {
                         </Fade>
                         <Fade bottom cascade delay={500}>
                             <ul className="mb-4 grid grid-cols-2 sm:grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4">
-                                {dish.ingredients.map((ingredient, j) => (
-                                    ingredients.map((item) => item == ingredient.name &&
+                                {
+                                    ingredients.map((ingredient) =>
                                         <li key={j}>
                                             <IngredientCard title={ingredient.name} quantity={`${ingredient.quantity[people - 1]} ${ingredient.unit}`} />
                                         </li>
                                     )
-                                ))}
+                                }
                             </ul>
                         </Fade>
                     </div>
